50f24b9b81be79d7d5c2d987cff2cf45cb602764
Nanci05 <92222413+Nanci05@users.noreply.github.com>
2023-03-02 15:47:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function click1() {
+    alert("Click me");
+}
+
+var req = new XMLHttpRequest()
+url = "https://restcountries.com/v3.1/all"
+
+req.open("GET", url, true)
+
+req.send()
+
+req.onload = function() {
+    //alert(this.response);
+    countries_data = Object.values(JSON.parse(this.response))
+        //alert(countries_data.length)
+        //alert(countries_data[0].name.common)
+    countries = []
+    for (var i = 0; i < countries_data.length; i++) {
+        countries.push("\n" + countries_data[i].name.common)
+    }
+    alert(countries)
+        //header = document.createElement("h1")
+        // header.innerHTML = countries[i]
+        //document.body.appendChild(header)
+}
